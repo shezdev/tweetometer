502cf4873acbe38d559db63cbc51e52924dc1d24
Irene Canuti <irene.canuti@gmail.com>
2017-02-28 20:20:10 +0000

    Implementing Twitter API calls, module pattern and callback

--- model/twitter.js
+++ model/twitter.js
@@ -1,13 +1,17 @@
 "use strict";
 
-var Twitter = require('twitter');
+var TwitterSearch = function(callback){
 
-var client = new Twitter({
-  consumer_key: 'xjGaJNZNiw0orjqBxXVZyT387',
-  consumer_secret: 'xSylmWQCarNzizPBagLbVxoJSpUEo5fI87UhSKzMTlQPtzTVd1',
-  bearer_token: 'AAAAAAAAAAAAAAAAAAAAANKtzQAAAAAACeVL2WBRi0vqO9k8Dx87Xf69UGU%3DdZfyGtv0d9J9xd195pS7j4ww0os4jyvgLpwbNTDuJ5fhBSAIDM'
-});
+  var Twitter = require('twitter');
 
-client.get('search/tweets', {q: 'eagle cat fox'}, function(error, tweets, response) {
-  console.log(tweets);
-});
+  var client = new Twitter({
+    consumer_key: 'xjGaJNZNiw0orjqBxXVZyT387',
+    consumer_secret: 'xSylmWQCarNzizPBagLbVxoJSpUEo5fI87UhSKzMTlQPtzTVd1',
+    bearer_token: 'AAAAAAAAAAAAAAAAAAAAANKtzQAAAAAACeVL2WBRi0vqO9k8Dx87Xf69UGU%3DdZfyGtv0d9J9xd195pS7j4ww0os4jyvgLpwbNTDuJ5fhBSAIDM'
+  });
+
+    client.get('search/tweets', {q: 'pancake'}, function(error, tweets, response) {
+      callback(tweets);
+    });
+
+};

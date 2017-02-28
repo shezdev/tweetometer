97add5c17bc2dab0d21614db8312d7fb964e262e
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-02-28 16:32:16 +0000

    can query twitter api

--- model/twitter.js
+++ model/twitter.js
@@ -1,9 +1,13 @@
 "use strict";
 
-var TwitterPackage = require('twitter');
+var Twitter = require('twitter');
 
 var client = new Twitter({
   consumer_key: 'xjGaJNZNiw0orjqBxXVZyT387',
   consumer_secret: 'xSylmWQCarNzizPBagLbVxoJSpUEo5fI87UhSKzMTlQPtzTVd1',
   bearer_token: 'AAAAAAAAAAAAAAAAAAAAANKtzQAAAAAACeVL2WBRi0vqO9k8Dx87Xf69UGU%3DdZfyGtv0d9J9xd195pS7j4ww0os4jyvgLpwbNTDuJ5fhBSAIDM'
+});
+
+client.get('search/tweets', {q: 'eagle cat fox'}, function(error, tweets, response) {
+  console.log(tweets);
 });

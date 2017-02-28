dc83dd20234cc9c90cb7016b7fb1700664301bc3
konradschlatte <konrad.schlatte@btinternet.com>
2017-02-28 17:48:28 +0000

    json_data

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var index = express();
 var path = require('path');
+//var dataParser = require('dataParser.js');
+
 
 index.set('views', path.join(__dirname, 'views'));
 index.engine('html', require('ejs').renderFile);
 index.set('view engine', 'html');
 index.use(express.static('public'));
 
+var dataParser = require('./dataParser');
+dataParser.read();
+var dataA = ["hello","goodbye"];
+
 index.get('/', function(req, res) {
   res.render('index.ejs');
 });
+
 
 index.listen(4000);
 console.log('server is running');

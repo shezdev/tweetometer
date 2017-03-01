2ecfb607365701e8dcfad89d1e4009a7770df943
konradschlatte <konrad.schlatte@btinternet.com>
2017-03-01 12:13:52 +0000

    added input field

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var index = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
 
 index.set('views', path.join(__dirname, 'views'));
 index.engine('html', require('ejs').renderFile);
 index.set('view engine', 'html');
 index.use(express.static('public'));
+index.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var dataParser = require('./dataParser');
 
 index.get('/', function(req, res) {
   res.render('index.ejs');
 });
-
+index.post("/", function (req, res) {
+ console.log(req.body.username);
+});
 
 index.listen(4000);
 console.log('server is running');

d49ecd6aeb1fb1ee2200a0c4d3e50cc06adcbe40
Ross Benzie <rossbenzie@gmail.com>
2017-03-01 14:21:37 +0000

    add some controller functions

--- tweetometer-controller.js
+++ tweetometer-controller.js
@@ -1,14 +1,28 @@
 $( document ).ready(function() {
-  // var thing = new Thing() // main object
+
+  // temp dummy
+  var Search = function() {
+    getResults: function(srchStrng) {return "<p>These are the results</p>";}
+  }
+  var search = new Search();
 
   $('#search-string').val("");
   $('#search-string').focus();
 
+  $('#search-btn').click(function() {
+    // get results
+    var results = search.getResults(('#search-string').val());
+    $('#results-box').val(results);
+    $('#search-string').focus();
+  });
 
   $('html').keypress(function (e) {
     var key = e.which;
     if(key == 13) {
-      submit();
+      // get results
+      var results = search.getResults(('#search-string').val());
+      $('#results-box').val(results);
+      $('#search-string').focus();
     }
-  });
+  }
 });

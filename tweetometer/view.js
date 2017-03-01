2ebb67eb7e9ad4fd8af419247ad49f31453e561c
konradschlatte <konrad.schlatte@btinternet.com>
2017-03-01 15:58:36 +0000

    thin processor

---
+++ tweetometer/view.js
@@ -0,0 +1,12 @@
+
+
+var view = (function(getData){
+  var result = document.getElementById('results-box');
+  console.log(result);
+  var dataA = getData.dataArray;
+  console.log(dataA);
+  for(i = 0;i <dataA.length; i++){result.insertAdjacentHTML("beforeend", '<li>' + dataA[i] + '</li>')};
+
+})(getData);
+
+// view(dataParser);

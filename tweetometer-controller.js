e4796a29a60c24eae9719cd8b5482d426f3c00c5
Ross Benzie <rossbenzie@gmail.com>
2017-03-01 17:00:04 +0000

    controller loop start and stop

--- tweetometer-controller.js
+++ tweetometer-controller.js
@@ -1,28 +1,41 @@
 $( document ).ready(function() {
 
-  // temp dummy
-  var Search = function() {
-    getResults: function(srchStrng) {return "<p>These are the results</p>";}
+  // temp dummy:
+  var search =  {
+    getResults: function(srchStrng) {return "<li>More results...</li>";}
   }
-  var search = new Search();
+
+  var stopSrch = false;
 
   $('#search-string').val("");
   $('#search-string').focus();
 
-  $('#search-btn').click(function() {
-    // get results
-    var results = search.getResults(('#search-string').val());
-    $('#results-box').val(results);
-    $('#search-string').focus();
+  $('#start-btn').click(function(event) {
+    event.preventDefault();
+    getResults();
+  });
+
+  $('#stop-btn').click(function(event) {
+    event.preventDefault();
+    stopSrch = true;
   });
 
   $('html').keypress(function (e) {
     var key = e.which;
     if(key == 13) {
-      // get results
-      var results = search.getResults(('#search-string').val());
-      $('#results-box').val(results);
-      $('#search-string').focus();
+      getResults();
+    }
+  });
+
+  function getResults() {
+    setInterval(printResults, 2000);
+    $('#search-string').focus();
+  }
+
+  function printResults() {
+    if (stopSrch == false) {
+      var srch = $('#search-string').val();
+      $('#results-list').append(search.getResults(srch));
     }
   }
 });

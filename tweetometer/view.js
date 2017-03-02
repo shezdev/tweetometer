d055049e142420a658de1d3a3613ccb4cc9a00e4
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-03-02 15:10:41 +0000

    change method of data output to that received from twitter API

--- tweetometer/view.js
+++ tweetometer/view.js
@@ -1,10 +1,30 @@
-var myVar = setInterval(function() { view(getData) }, 1000);
+// call Search.getTweets(string) and view(result)
+// result will be in format timestamp { string: count }
+// use listener, or
+//
+// function returnHTML(array){
+//   var result = document.getElementById('results-box');
+//   console.log(result);
+//   var dataA = getData.dataArray;
+//   console.log(dataA);
+//   for(i = 0;i <dataA.length; i++){result.insertAdjacentHTML("beforeend", '<li>' + dataA[i] + '</li>')};
+// };
 
-  function view(getData){
+var Control = (function(getData) {
+
+  var getTweets = function(hash) {
+    getTweets(hash);
+  };
+
+  var returnHTML = function(getData) {
     var result = document.getElementById('results-box');
-    console.log(result);
-    var dataA = getData.dataArray;
-    console.log(dataA);
-    for(i = 0;i <dataA.length; i++){result.insertAdjacentHTML("beforeend", '<li>' + dataA[i] + '</li>')};
+    result.insertAdjacentHTML("beforeend", '<li>' + getData.timestamp(hash) + ': ' + getData.nMentions(hash) + '</li>')
+  };
 
-  }(getData);
+  var stopStream = function(arguments) {
+
+  }
+  return { returnHTML:returnHTML }
+})();
+
+Control.returnHTML(getData);

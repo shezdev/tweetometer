ca2876cab20923ee4322e0b3b51f768284ca97f0
Ross Benzie <rossbenzie@gmail.com>
2017-03-03 11:45:22 +0000

    further ajax/node

--- tweetometer.js
+++ tweetometer.js
@@ -4,30 +4,12 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-
-
 http.createServer( function (request, response) {
 
   var pathname = url.parse(request.url).pathname;
   var params = url.parse(request.url).params;
   console.log("Request for " + pathname + " received.");
 
-  // if (pathname == "/index.html") {
-  //   loadHomepage();
-  // } else if (pathname.substr(1) == "start") {
-  //   var searchString = pathname.substr(3);
-  //   tweetometerSearch(searchString);
-  // }
-  //
-  // function tweetometerSearch(searchString) {
-  //   var tweetometerResults = Search.getTweets(searchString);
-  //   response.writeHead(200,  {'Content-Type': 'text/html'});
-  //   response.write("dummy results");
-  //   // response.write(tweetometerResults);
-  //   response.end();
-  // }
-
-  // function loadHomepage() {
     fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
         console.log(err);
